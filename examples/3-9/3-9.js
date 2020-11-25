b7ad970c1c2cd5b174499c8ec3fa22f67358054f
vexillaregis <vexillaregis@users.github.com>
2020-11-25 23:35:44 +0800

    Create 3-9.js

---
+++ examples/3-9/3-9.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+	var allDivs = document.getElementsByTagName('div');
+	console.log(allDivs[0]);
+	var allAudios = document.getElementsByTagName('audio');
+	console.log(allAudios[0]);
+};
