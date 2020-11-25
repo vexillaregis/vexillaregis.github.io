c3fde8f0fd7c67fe667d66ab47d9435a2b254c1f
vexillaregis <vexillaregis@users.github.com>
2020-11-25 23:36:03 +0800

    Create 3-1.js

---
+++ quizzes/3-1/3-1.js
@@ -0,0 +1,16 @@
+var longArray = [78, 12, 5, 6, 19, 67, 1, -56, 10];
+
+// Replace the thrid and seventh value with 100 and 1000 respectively
+
+longArray[2] = 100;
+longArray[6] = 1000;
+
+console.log(longArray);
+
+// Calculate the product of the first, second, and seventh values
+
+console.log(longArray[0] * longArray[1] * longArray[6]);
+
+// Divide the last value by the first three values
+
+console.log(longArray[8] / longArray[0] / longArray[1] / longArray[2]);
