a8e19beb0a9e9f85e79c3d70604876d7fcc5b715
vexillaregis <vexillaregis@users.github.com>
2020-11-25 23:35:08 +0800

    Create 3-2.js

---
+++ examples/3-2/3-2.js
@@ -0,0 +1,43 @@
+//var num;
+//num = 5;
+var num = 5;
+
+//console.log(num);
+
+var x = 10, y = 20, z = 30;
+
+/*
+console.log(x);
+console.log(y);
+console.log(z);
+*/
+
+var undefinedVar;
+
+//console.log(undefinedVar);
+
+//console.log(him);
+//var him = 'Mike';
+
+var him;
+//console.log(him);
+him = 'Mike';
+
+var newNum = 0;
+//console.log(newNum); // Output = 0
+newNum = 5;
+//console(newNum); // Output = 5
+
+var randomContent = 5;
+//console.log(randomContent); //Output = 5
+randomContent = 'This is a string.';
+//console.log(randomContent); //Output = This is a string.
+
+//var randomNumber, number, randomNumber;
+//var var;
+var ಠ_ಠ = 5;
+//console.log(ಠ_ಠ);
+
+var a = 5, A = 'Upper case!';
+console.log(a);
+console.log(A);
